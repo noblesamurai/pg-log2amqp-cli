320c87443b6eb16af22fe15e591f35bcbab7ce0e
Andrew Harris <andrew@noblesamurai.com>
2019-02-20 15:10:55 +1100

    get it mostly working

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-const knex = require('knex')(process.env.DATABASE_URL);
-const knexLog = require('knex-log')(knex, { tableName: 'image-search-logs', columnName: 'data' });
+const config = require('./config');
+const knex = require('knex')(config.db);
+const knexLog = require('knex-log')(knex, { tableName: config.tableName, columnName: 'data' });
 const amqp = require('amqplib');
-const transferStreamBackpressure = require('transfer-stream-backpressure');
-let conn;
+const AmqpWritableStream = require('./amqp-writable-stream');
+
+main();
 
 async function main () {
   try {
-    conn = await amqp.connect(process.env.AMQP_URL);
-    const rs = knexLog.createReadStream();
-    const check = () => {};
-    const options = {};
-    const through = transferStreamBackpressure(check, options);
-    const write = amqpWrite(/* FIXME... */);
-    rs.pipe(through).pipe(write);
-    rs.on('error', fail);
+    // create knex log stream
+    const rs = knexLog.createReadStream({ offset: { id: config.logOffset } });
+
+    // create amqp connection and write stream
+    const conn = await amqp.connect(config.amqp.url);
+    const ch = await conn.createChannel();
+    // just check that the queue exists... will throw if it doesn't. the main logging process
+    // should be responsible for making sure it exists.
+    await ch.checkQueue(config.amqp.queueName);
+    const write = new AmqpWritableStream(ch, config.amqp.queueName);
+
+    // begin...
+    rs.pipe(write).on('error', fail);
+    write.on('finish', end);
   } catch (err) {
     fail(err);
   }
 }
 
-main();
-
 function fail (err) {
   console.error('failed', err);
   process.exit(1);
+}
+
+function end () {
+  process.exit(0);
 }
