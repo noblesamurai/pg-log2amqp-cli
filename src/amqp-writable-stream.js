b475ac46719d8675ae43b8485c13b23097b62127
Andrew Harris <andrew@noblesamurai.com>
2019-02-22 15:21:48 +1100

    add flow control comment to code

--- src/amqp-writable-stream.js
+++ src/amqp-writable-stream.js
@@ -1,6 +1,9 @@
 const { Writable } = require('stream');
 const stringify = require('json-stringify-safe');
 
+/**
+ * @see http://www.squaremobius.net/amqp.node/channel_api.html#flowcontrol
+ */
 module.exports = class AmqpWritableStream extends Writable {
   constructor (channel, queueName, opts = {}) {
     super(Object.assign(opts, { objectMode: true }));
