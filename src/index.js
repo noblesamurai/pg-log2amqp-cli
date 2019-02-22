76a34171daa479693eac592b08ea45d8e59cee5f
Andrew Harris <andrew@noblesamurai.com>
2019-02-22 14:55:46 +1100

    more progress out

--- src/index.js
+++ src/index.js
@@ -3,27 +3,14 @@ const knex = require('knex')(config.db);
 const knexLog = require('knex-log')(knex, { tableName: config.tableName, columnName: 'data' });
 const amqp = require('amqplib');
 const AmqpWritableStream = require('./amqp-writable-stream');
-const progress = require('progress-stream');
-const { LogFrame, CompositeLogView, RawLogView } = require('log-frame');
-const { ProgressBar } = require('logf-progress');
+const getProgressStream = require('./get-progress-stream');
 
 main();
 
 async function main () {
   try {
     const { count } = await knex(config.tableName).where('id', '>=', config.logOffset).count().first();
-    const ps = progress({ objectMode: true, length: parseInt(count), time: 1000 });
-    const view = new CompositeLogView();
-    const frame = new LogFrame({ hideCursor: true });
-    frame.view = view;
-    const bar = new ProgressBar();
-    const label = new RawLogView();
-    view.addChild(bar);
-    view.addChild(label);
-    ps.on('progress', p => {
-      bar.setProgress(p.percentage / 100);
-      label.content = ` - ${p.percentage.toFixed(2)}% ${p.length - p.remaining}/${p.length}`;
-    });
+    const { stream: ps, complete } = getProgressStream(count);
 
     // create knex log stream
     const rs = knexLog.createReadStream({
@@ -42,7 +29,10 @@ async function main () {
 
     // begin...
     rs.pipe(ps).pipe(write).on('error', fail);
-    write.on('finish', end);
+    write.on('finish', () => {
+      complete();
+      setTimeout(end, 1000); // delay to let progress loop catchup.
+    });
   } catch (err) {
     fail(err);
   }
